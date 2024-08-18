9b35f4457ec00dec3dbf0950bf2c384f9b4dbeae
Ha Cuong Thinh <thinhhacuong123@gmail.com>
2024-08-18 23:15:09 +0700

    finish profile page

--- client/src/component/Projects/index.js
+++ client/src/component/Projects/index.js
@@ -1,9 +1,83 @@
-function Projects (){
-    return (
-        <>
-            Projects
-        </>
-    )
+import { Timeline } from "antd";
+import "./Projects.scss";
+
+function Projects() {
+  return (
+    <>
+      <div style={{marginTop: "20px"}}></div>
+      <Timeline
+        pending="To be continue..."
+        items={[
+          {
+            color: "#FF7F50",
+            children: (
+              <>
+                <div class="box">
+                    <h4>LANDING PAGE: SoftwareM4</h4>
+                    <h4 class="year">- 2022 -</h4>
+                </div>
+                <p>Tech: HTML5, CSS3</p>
+                <a href="https://project-mini-1-phi.vercel.app/" target="_blank" rel="noreferrer" class="link">Demo</a>
+              </>
+            ),
+          },
+          {
+            color: "#FF7F50",
+            children: (
+              <>
+                <div class="box"> 
+                    <h4>LANDING PAGE: Softy Pinko</h4>
+                    <h4 class="year">- 2022 -</h4>
+                </div>
+                <p>Tech: HTML5, CSS3, Bootstrap4</p>
+                <a href="https://project-mini-2-tawny.vercel.app/" target="_blank" rel="noreferrer" class="link">Demo</a>
+              </>
+            ),
+          },
+          {
+            color: "#FF7F50",
+            children: (
+              <>
+                <div class="box"> 
+                    <h4>WEBSITE: Football Forum & Shop</h4>
+                    <h4 class="year">- 2023 -</h4>
+                </div>
+                <p>Tech: ReactJS, NodeJS</p>
+                <a href="https://github.com/hcthinhjr03/BTLJava2023" target="_blank" rel="noreferrer" class="link">Source</a>
+              </>
+            ),
+          },
+          {
+            color: "#FF7F50",
+            children: (
+              <>
+                <div class="box"> 
+                    <h4>WEBSITE: Photo Sharing</h4>
+                    <h4 class="year">- 2024 -</h4>
+                </div>
+                <p>Tech: ReactJS, ExpressJS, MongoDB</p>
+                <a href="https://github.com/hcthinhjr03/project6-client" target="_blank" rel="noreferrer" class="link">Source FE</a>
+                <a href="https://github.com/hcthinhjr03/project6-server" target="_blank" rel="noreferrer" class="link">Source BE</a>
+              </>
+            ),
+          },
+          {
+            color: "#FF7F50",
+            children: (
+              <>
+                <div class="box"> 
+                    <h4>MOBILE APP: Property Manager</h4>
+                    <h4 class="year">- 2024 -</h4>
+                </div>
+                <p>Tech:  React Native, ExpressJS, MongoDB</p>
+                <a href="https://github.com/hcthinhjr03/qlts_app" target="_blank" rel="noreferrer" class="link">Source</a>
+              </>
+            ),
+          }
+        ]}
+      />
+    </>
+  );
 }
 
 export default Projects;
--- client/src/component/Skills/index.js
+++ client/src/component/Skills/index.js
@@ -1,9 +1,92 @@
-function Skills (){
-    return (
-        <>
-            Skills
-        </>
-    )
+import { ConfigProvider, Tree } from "antd";
+const { DirectoryTree } = Tree;
+const treeData = [
+  {
+    title: "Professional skills",
+    key: "0-0",
+    children: [
+      {
+        title: "HTML, CSS, JavaScript",
+        key: "0-0-0",
+        isLeaf: true,
+      },
+      {
+        title: "NodeJS, ExpressJS",
+        key: "0-0-1",
+        isLeaf: true,
+      },
+      {
+        title: "React Native , ReactJS",
+        key: "0-0-2",
+        isLeaf: true,
+      },
+      {
+        title: "MongoDB, MySQL",
+        key: "0-0-3",
+        isLeaf: true,
+      },
+      {
+        title: "Git, GitHub",
+        key: "0-0-4",
+        isLeaf: true,
+      },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    key: "0-1",
+    children: [
+      {
+        title: "Problem Solving",
+        key: "0-1-0",
+        isLeaf: true,
+      },
+      {
+        title: "Teamwork",
+        key: "0-1-1",
+        isLeaf: true,
+      },
+      {
+        title: "Time Management",
+        key: "0-1-2",
+        isLeaf: true,
+      },
+      {
+        title: "Self-study",
+        key: "0-1-3",
+        isLeaf: true,
+      },
+    ],
+  },
+];
+
+function Skills() {
+  const onExpand = (keys, info) => {
+    console.log("Trigger Expand", keys, info);
+  };
+  return (
+    <>
+      <ConfigProvider
+      theme={{
+        token: {
+            fontSize: 18,
+            lineHeight: 2
+        },
+        components: {
+            Tree: {
+                titleHeight: 30
+            }
+        }
+      }}>
+        <DirectoryTree
+          defaultExpandAll
+          onExpand={onExpand}
+          treeData={treeData}
+          selectable={false}
+        />
+      </ConfigProvider>
+    </>
+  );
 }
 
 export default Skills;
